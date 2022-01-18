1fd90164e79421e87d62b6e4888b38528e8aa140
Andrew Kump <kumpandrew@gmail.com>
2022-01-18 14:33:34 -0500

    remove console.log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch';
 const [word] = process.argv.slice(2);
 
-console.log(word)
 const wordle = async function(){
     const res = await fetch(` https://api.memecenter.org/words/verify/${word}`);
     const data = await res.json();
