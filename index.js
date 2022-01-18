7229e12a275ddd2afdbbada5c2ed8016e7c77957
Andrew Kump <kumpandrew@gmail.com>
2022-01-18 14:35:06 -0500

    remove whitespace

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const [word] = process.argv.slice(2);
 
 const wordle = async function(){
-    const res = await fetch(` https://api.memecenter.org/words/verify/${word}`);
+    const res = await fetch(`https://api.memecenter.org/words/verify/${word}`);
     const data = await res.json();
     console.log(data)
 }
